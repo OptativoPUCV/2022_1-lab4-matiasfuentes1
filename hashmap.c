b79de22174456e4cfd85cd398cedb62576387e16
Your Name <you@example.com>
2022-04-10 20:45:11 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -137,12 +137,12 @@ Pair * nextMap(HashMap * map) {
     }
     while(map -> buckets[indice] == NULL || map -> buckets[indice] -> key == NULL){
         indice = (indice+1) % map -> capacity; 
-        return map -> current = indice;
+        map -> current = indice;
         return map -> buckets[indice];
     }
    
-    map -> current = indice;
-    return map -> buckets[indice];
-
+    //map -> current = indice;
+    //return map -> buckets[indice];
+      return NULL;
     
 }
